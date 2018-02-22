789d0ff42e6e3845d76d9ed4fe7c9dc70fd6c266
Seshadri Ganjur <seshadriganjur@Seshadris-MacBook-Pro.local>
2018-02-22 20:38:31 +0530

    Added Support for additional factors
    Support for Modi Factor
    Support for Anti Incumbancy Factor

--- KAAssMap/KAUtils.py
+++ KAAssMap/KAUtils.py
@@ -437,7 +437,7 @@ def bsrcp2bjp(fdf,pct):
 
 def inc2bjp(fdf,pct):
     df = fdf.copy()
-    transfer_protocol={"from":"INC","to":"BJP","percent":2}
+    transfer_protocol={"from":"INC","to":"BJP","percent":pct}
     
     tdf = transferVotes(df,transfer_protocol)
     tdf = reassignPositions(tdf)
@@ -446,9 +446,296 @@ def inc2bjp(fdf,pct):
 
 def jds2bjp(fdf,pct):
     df = fdf.copy()
-    transfer_protocol={"from":"JD(S)","to":"BJP","percent":2}
+    transfer_protocol={"from":"JD(S)","to":"BJP","percent":pct}
     
     tdf = transferVotes(df,transfer_protocol)
     tdf = reassignPositions(tdf)
     
     return tdf
+
+def inc2jds(fdf,pct):
+    df = fdf.copy()
+    transfer_protocol={"from":"INC","to":"JD(S)","percent":pct}
+    
+    tdf = transferVotes(df,transfer_protocol)
+    tdf = reassignPositions(tdf)
+    
+    return tdf
+
+def ind2bjp(fdf,pct):
+    df = fdf.copy()
+    transfer_protocol={"from":"IND","to":"BJP","percent":pct}
+    
+    tdf = transferVotes(df,transfer_protocol)
+    tdf = reassignPositions(tdf)
+    
+    return tdf
+
+
+
+def get2018Results(df2018):
+        
+    rdf = df2018
+    year = 2018
+    ac_names = getAC_Names()
+    
+    winners = []
+    winnerParties = []
+    winnerVotes = []
+    
+    runners = []
+    runnerParties = []
+    runnerVotes = []
+    
+    winDF = rdf.loc[(rdf['YEAR']==year) & (rdf['POSITION']==1)]
+    runDF = rdf.loc[(rdf['YEAR']==year) & (rdf['POSITION']==2)]
+    
+    for ac_name in ac_names:
+        ac_name = removeSCSTFromName(ac_name)
+        winner = np.nan
+        winnerParty = np.nan
+        winnerVote = np.nan
+        runner = np.nan
+        runnerParty = np.nan
+        runnerVote = np.nan
+        
+        tdf = winDF.loc[winDF['AC_NAME']==ac_name]
+        if len(tdf.index) > 0:
+            winner = tdf['NAME'].values[0]
+            winnerParty = tdf['PARTY'].values[0]
+            winnerVote = tdf['VOTES'].values[0]
+            if np.isnan(winnerVote) == False:
+                winnerVote = int(winnerVote)               
+                    
+        
+        tdf = runDF.loc[runDF['AC_NAME']==ac_name]
+        if len(tdf.index) > 0:
+            runner = tdf['NAME'].values[0]
+            runnerParty = tdf['PARTY'].values[0]
+            runnerVote = tdf['VOTES'].values[0]
+            if np.isnan(runnerVote) == False:
+                runnerVote = int(runnerVote)
+                
+        
+        winners.append(winner)
+        winnerParties.append(winnerParty)
+        winnerVotes.append(winnerVote)
+
+        runners.append(runner)
+        runnerParties.append(runnerParty)
+        runnerVotes.append(runnerVote)
+        
+        winnerVotes = [-99999 if np.isnan(x) else x for x in winnerVotes]
+        runnerVotes = [-999999 if np.isnan(x) else x for x in runnerVotes]
+    
+    results = {"Winners":winners,\
+               "Winner Parties": winnerParties,"Winner Votes":winnerVotes, \
+               "Runners":runners,"Runner Parties": runnerParties,\
+               "Runner Votes":runnerVotes}
+    return results
+
+
+# This function returns Dataframe with party results data for the year
+def get2018KAPartyResults(df2018):
+    
+    year = 2018
+    rdf = df2018
+    
+    #First get the results for a specific year
+    yeardf = rdf.loc[rdf['YEAR'] == year]
+    
+    #Now that we have the results for the year
+    #Let's start gathering some information and stats
+    
+    #Total Votes Cast in that election
+    total_votes = int(yeardf['VOTES'].sum())
+    
+    #Parties in that year
+    parties = yeardf['PARTY'].unique()
+    
+    #Let us get the PCTMargins for that year
+    marginsDF = getPCTMarginsDFByYear(year)
+    
+    #Now that we have the parties for the year,
+    #Let us start gathering stats for the parties
+    election_years = []
+    seats_contested = []
+    votes_won = []
+    seats_won = []
+    conversion_ratio = []
+    percent_votes = []
+    median_margin = []
+    median_pct_margin = []
+    women_cands = []
+    women_winners = []
+    std_margin = []
+    std_pct_margin = []
+    overall_median_margin = []
+    overall_median_pct_margin = []
+    overall_std_margin = []
+    overall_std_pct_margin = []
+    
+    ovr_med_marg = marginsDF['WINNING_MARGIN'].median()
+    ovr_med_pct_marg = marginsDF['PCT_MARGIN'].median()
+    ovr_std_marg = marginsDF['WINNING_MARGIN'].std()
+    ovr_std_pct_marg = marginsDF['PCT_MARGIN'].std()
+    
+    
+    for party in parties:
+        
+        election_years.append(year)
+        
+        contested = len(yeardf.loc[yeardf['PARTY']==party].index)
+        seats_contested.append(contested)
+        
+        votes = yeardf.loc[yeardf['PARTY']==party]['VOTES'].sum()
+        votes_won.append(int(votes))
+
+        seats = len (yeardf.loc[(yeardf['PARTY'] == party) & (yeardf['POSITION'] == 1)].index)
+        seats_won.append(seats)
+        
+        conversion_ratio.append(seats/votes)
+        percent_votes.append(votes/total_votes * 100)
+        
+        cands = len (yeardf.loc[(yeardf['PARTY'] == party) & (yeardf['SEX'] == 'F')].index)
+        women_cands.append(cands)
+        
+        winners = len (yeardf.loc[(yeardf['PARTY'] == party) & (yeardf['SEX'] == 'F') & (yeardf['POSITION'] == 1)].index)
+        women_winners.append(winners)
+        
+        #Let us calculate the medians
+        med_marg = -999999
+        med_pct = -9999999
+        std_marg = -999999
+        std_pct = -9999999
+        
+        #Get the subset for the party from marginsDF
+        subDF = marginsDF.loc[marginsDF['WINNER_PARTY'] == party]
+        
+        if subDF is not None:
+            med_marg = subDF['WINNING_MARGIN'].median()
+            med_pct = subDF['PCT_MARGIN'].median()
+            std_marg = subDF['WINNING_MARGIN'].std()
+            std_pct = subDF['PCT_MARGIN'].std()
+            
+        median_margin.append(med_marg)
+        median_pct_margin.append(med_pct)
+        std_margin.append(std_marg)
+        std_pct_margin.append(std_pct)
+        overall_median_margin.append(ovr_med_marg)
+        overall_median_pct_margin.append(ovr_med_pct_marg)
+        overall_std_margin.append(ovr_std_marg)
+        overall_std_pct_margin.append(ovr_std_pct_marg)
+    
+    #Now lets create a dataframe out of these lists
+    #First create a list of tuples with column names and values
+    #And the use that list of tuples to create the DF
+    
+    results = [('YEAR',election_years),
+              ('PARTY',parties),
+              ('CONTESTED',seats_contested),
+              ('SEATS_WON',seats_won),
+              ('VOTES_WON',votes_won),
+              ('PCT_VOTES_WON',percent_votes),
+              ('CONVERSION_RATIO',conversion_ratio),
+              ('MEDIAN_MARGIN',median_margin),
+              ('MEDIAN_PCT_MARGIN',median_pct_margin),
+              ('STD_MARGIN',std_margin),
+              ('STD_PCT_MARGIN',std_pct_margin),
+              ('OVERALL_MEDIAN_MARGIN',overall_median_margin),
+              ('OVERALL_MEDIAN_PCT_MARGIN',overall_median_pct_margin),
+              ('OVERALL_STD_MARGIN',overall_std_margin),
+              ('OVERALL_STD_PCT_MARGIN',overall_std_pct_margin),
+              ('WOMEN_CANDS',women_cands),
+              ('WOMEN_WINNERS',women_winners)
+              ]
+    
+    results = pd.DataFrame.from_items(results)
+    
+    return results 
+
+
+def get2018ContestedWinners(df2018):
+    
+    df = df2018.copy()
+    year = 2018
+    
+    #Remove those who won unopposed
+    df = df.loc[(df['YEAR']==year) & (df['POSITION'] == 1) & (df['VOTES'].notnull())]
+    
+    #remove the POSITION column
+    del df['POSITION']
+    
+    #rename columns
+    df.rename(columns={'NAME':'WINNER',"SEX":"WINNER_SEX", \
+                       "AGE":"WINNER_AGE",\
+                       "CATEGORY":"WINNER_CATEGORY","PARTY":"WINNER_PARTY",\
+                       "VOTES":"WINNER_VOTES"\
+                      }, inplace=True)
+    
+    return df
+
+# Returns the dataframe of all Runners up for the year
+def get2018RunnersDF(df2018):
+    
+    df = df2018.copy()
+    year = 2018
+    
+    #Remove those who won unopposed
+    df = df.loc[(df['YEAR']==year) & (df['POSITION'] == 2) & (df['VOTES'].notnull())]
+    
+    #remove the POSITION column
+    del df['POSITION']
+    
+    #rename columns
+    df.rename(columns={'NAME':'RUNNER',"SEX":"RUNNER_SEX", "AGE":"RUNNER_AGE",\
+                          "CATEGORY":"RUNNER_CATEGORY","PARTY":"RUNNER_PARTY","VOTES":"RUNNER_VOTES"\
+                         }, inplace=True)
+    
+    return df
+
+
+# Returns the dataframe of results with the winning margin for the specified year
+def get2018MarginsDF(df2018):
+    year = 2018
+    winDF = get2018ContestedWinners(df2018)
+        
+    runDF = get2018RunnersDF(df2018)
+    
+    
+    #merge the two dataframes by their common columns
+    marginDF = pd.merge(winDF,runDF,on=["ST_NAME","YEAR","AC_NO","AC_NAME","AC_TYPE"])
+    
+    #We will now calculate the margin and assign it to a new column
+    marginDF["WINNING_MARGIN"]= marginDF["WINNER_VOTES"]- marginDF["RUNNER_VOTES"]
+    
+    return marginDF
+    
+
+# Returns the dataframe of results with the pct winning margin for the specified year
+def get2018PCTMarginsDF(df2018):
+    df = df2018.copy()
+    year = 2018
+    
+    #Remove those rows where there no values for VOTES
+    #Get data for only the relevant year
+    df = df.loc[(df['YEAR']==year) & (df['VOTES'].notnull())]
+    
+    totVotes = []
+    counter=0
+    
+    #Get the Margins DF for the year
+    marginDF = get2018MarginsDF(df)
+    
+    for index, row in marginDF.iterrows():
+        counter += 1
+        ac_name=row['AC_NAME']
+        subDF = df.loc[(df['YEAR'] == year) & (df['AC_NAME'] == ac_name)]
+        totalVotes = subDF['VOTES'].sum()
+        totVotes.append(totalVotes)
+        
+    marginDF['TOTAL_VOTES']=totVotes
+    marginDF['PCT_MARGIN']=marginDF['WINNING_MARGIN']/marginDF['TOTAL_VOTES']*100
+
+    return(marginDF)
+
--- KAAssMap/WIPCharts.py
+++ KAAssMap/WIPCharts.py
@@ -14,6 +14,9 @@ from bokeh.models import (
 
 def update_party_data(year):
 
+    if year == 2018:
+        return
+
     df = party_results[year]
 
     parties = df.loc[df['SEATS_WON']>0]['PARTY'].tolist()
@@ -39,6 +42,8 @@ def update_party_data(year):
 
 
 def update_maps_data(year):
+    if year == 2018:
+        return
 
     results = map_results[year]
     MapSource.data["winner"] = results["Winners"]
@@ -50,14 +55,110 @@ def update_maps_data(year):
 
 
 def update_box_data(year):
+
+    if year == 2018:
+        return
+
     box_source.data = box_dic[year]
     box_plot.x_range.factors = box_source.data['x']
+
+
+
+def update_2018_data():
+
+
+
+    #First let's get the full results dataframe for 2018
+    df2018 = KAU.getKAResultsByYear(2018)
+
+
+
+    #Get the value from modi_slider and set it as modi_pct
+    modi_pct = float(modi_slider.value)
+
+    #Transfer votes from various parties to BJP based on modi_pct
+    if modi_pct > 0.0:
+        df2018 = KAU.inc2bjp(df2018,modi_pct)
+        df2018 = KAU.jds2bjp(df2018,modi_pct)
+        #df2018 = KAU.ind2bjp(df2018,modi_pct)
+
+
+    #Get the value from anti_inc_slider and set it as anti_inc_pct
+    anti_inc_pct = float(anti_inc_slider.value)
+    if anti_inc_pct > 0.0:
+        df2018 = KAU.inc2bjp(df2018,anti_inc_pct * 2/3)
+        df2018 = KAU.inc2jds(df2018,anti_inc_pct * 1/3)
+
+    
+
+    #Reassign positions based on the votes
+    try:
+        df2018 = KAU.reassignPositions(df2018)
+    except Exception :
+        print("Exception in reassignPositions")
+    else:
+        pass
+    finally:
+        pass
+    
+    #Table display stuff
+    rdf2018 = KAU.get2018KAPartyResults(df2018)
+
+    party_results2018=rdf2018
+    tblStuff2018 = KAC.getKAPartyTblStuff(rdf2018)
+    tbl_dic2018 = tblStuff2018
+
+    tblSource.data = tbl_dic2018['source'].data
+
+    #Map Stuff
+    results2018 = KAU.get2018Results(df2018)
+    
+    MapSource.data["winner"] = results2018["Winners"]
+    MapSource.data["winnerParty"] = results2018["Winner Parties"]
+    MapSource.data["runner"] = results2018["Runners"]
+    MapSource.data["runnerParty"] = results2018["Runner Parties"]
+    MapSource.data["winnerVotes"] = results2018["Winner Votes"]
+    MapSource.data["runnerVotes"] = results2018["Runner Votes"]
+
+
+    #Box Plot Stuff
+    bdf2018 = KAU.get2018PCTMarginsDF(df2018)
+    rdf2018 = bdf2018[['WINNER_PARTY',"PCT_MARGIN"]]
+    box_data2018 = KAC.buildBoxPlotData(rdf2018)
+    box_plot.x_range.factors = box_data2018['x']
+    box_source.data = box_data2018
+
+
+    #Donut chart
+    tmpdf = party_results2018
+    parties2018 = tmpdf.loc[tmpdf['SEATS_WON']>0]['PARTY'].tolist()
+    party_dic2018 = {"Party":parties}
+    seats = tmpdf.loc[tmpdf['SEATS_WON']>0]['SEATS_WON'].tolist()
+    seats_dic = {"Seats":seats}
+
+    sangles,eangles = KAC.getStartsEnds(seats)
+    colors = KAC.getPartyColors(parties)
+
+    dnsource1.data['labels']=parties
+    dnsource1.data['amounts']=seats
+    dnsource1.data['starts']=sangles
+    dnsource1.data['ends']=eangles
+    dnsource1.data['colors']=colors
+    
+    
+
+    
+    return
 
 
 def update_data(attrname, old, new):
 
     # Get the current slider values
     year = year_slider.value
+
+    if year == 2018:
+        update_2018_data()
+        return
 
     if year in party_results.keys():
         update_party_data(year)
@@ -69,6 +170,15 @@ def update_data(attrname, old, new):
     if year in box_dic:
         update_box_data(year)
     	
+
+
+
+
+
+#######################################
+#######################################
+############ Main Stuff Begins
+##################################
 
 # fetch and clear the document
 from bokeh.io import curdoc
@@ -129,14 +239,21 @@ dnplt2, dnsource2 = KAC.DonutChart(party_dic,votes_pct_dic,"% Votes Shares")
 year_slider = Slider(start=1957, end=2018, value=2008, step=1,title="Year")
 year_slider.on_change('value', update_data)
 
+modi_slider = Slider(start=0.0, end=5.0, value=0.0, step=1,title="Modi Factor")
+modi_slider.on_change('value', update_data)
+
+anti_inc_slider = Slider(start=0.0, end=3.0, value=0.0, step=1,title="Anti Incumbency Factor")
+anti_inc_slider.on_change('value', update_data)
+
+sliders = column([year_slider,modi_slider,anti_inc_slider])
 from bokeh.layouts import gridplot
 
 donuts = gridplot([tblPlt,dnplt1],ncols=2,plot_width=275, plot_height=275,toolbar_location="right")
 donuts_box = column(donuts, box_plot)
-map_plus_slider = column([MapPlot, year_slider])
+map_plus_sliders = column([MapPlot, sliders])
 
 
-master = row([map_plus_slider,donuts_box])
+master = row([map_plus_sliders,donuts_box])
 curdoc().add_root(master)
 #curdoc().add_root(column(MapPlot,donuts_plus_slider))
 curdoc().title = "Karnataka Election Results"
